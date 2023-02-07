e71b982c5a3d8d6b895c9dad2e3d6c5ea88588ef
Aman <amankhan.pathan@nexturn.com>
2023-02-07 14:18:01 +0530

    Updating

---
+++ Cal/src/main/java/pkgCalculator/Calculator.java
@@ -0,0 +1,19 @@
+package pkgCalculator;
+
+public class Calculator {
+	int a,b;
+	public int addition(int a,int b) {
+		return a+b;
+	}
+	public int subtraction(int a,int b) {
+		return a-b;
+	}
+	public int multiplication(int a,int b) {
+		return a*b;
+	}
+	public int division(int a,int b) {
+		return a/b;
+	}
+
+
+}
