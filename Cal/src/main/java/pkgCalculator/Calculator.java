487f9bf466b00c9a240fc7198228ce8c3eaa38da
amankhan1847 <118450153+amankhan1847@users.noreply.github.com>
2023-07-10 16:27:17 +0530

    Update Calculator.java

--- Cal/src/main/java/pkgCalculator/Calculator.java
+++ Cal/src/main/java/pkgCalculator/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
 	public int addition(int a,int b) {
 		return a+b;
 	}
-	public int subtraction(int a,int b) {
+	public int subtraction(int a,int b) {  //abc 
 		return a-b;
 	}
 	public int multiplication(int a,int b) {
